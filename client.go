954ed3a9e67bf0b905f34171702d236a811cb774
Marten Seemann <martenseemann@gmail.com>
2023-05-03 12:12:19 +0200

    log upload and download times and calculate bandwidth

--- client.go
+++ client.go
@@ -5,6 +5,8 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
+	"log"
+	"time"
 
 	"github.com/quic-go/quic-go"
 )
@@ -25,39 +27,48 @@ func RunClient(addr string, uploadBytes, downloadBytes uint64) error {
 	if err != nil {
 		return err
 	}
-	return handleClientStream(str, uploadBytes, downloadBytes)
+	uploadTook, downloadTook, err := handleClientStream(str, uploadBytes, downloadBytes)
+	if err != nil {
+		return err
+	}
+	log.Printf("uploaded %s: %.2fs (%s/s)", formatBytes(uploadBytes), uploadTook.Seconds(), formatBytes(bandwidth(uploadBytes, uploadTook)))
+	log.Printf("downloaded %s: %.2fs (%s/s)", formatBytes(downloadBytes), downloadTook.Seconds(), formatBytes(bandwidth(downloadBytes, downloadTook)))
+	return nil
 }
 
-func handleClientStream(str io.ReadWriteCloser, uploadBytes, downloadBytes uint64) error {
+func handleClientStream(str io.ReadWriteCloser, uploadBytes, downloadBytes uint64) (uploadTook, downloadTook time.Duration, err error) {
 	b := make([]byte, 8)
 	binary.BigEndian.PutUint64(b, downloadBytes)
 	if _, err := str.Write(b); err != nil {
-		return err
+		return 0, 0, err
 	}
 
 	// upload data
 	b = make([]byte, 16*1024)
+	uploadStart := time.Now()
 	for uploadBytes > 0 {
 		if uploadBytes < uint64(len(b)) {
 			b = b[:uploadBytes]
 		}
 		n, err := str.Write(b)
 		if err != nil {
-			return err
+			return 0, 0, err
 		}
 		uploadBytes -= uint64(n)
 	}
 	if err := str.Close(); err != nil {
-		return err
+		return 0, 0, err
 	}
+	uploadTook = time.Since(uploadStart)
 
 	// download data
 	b = b[:cap(b)]
 	remaining := downloadBytes
+	downloadStart := time.Now()
 	for remaining > 0 {
 		n, err := str.Read(b)
 		if uint64(n) > remaining {
-			return fmt.Errorf("server sent more data than expected, expected %d, got %d", downloadBytes, remaining+uint64(n))
+			return 0, 0, fmt.Errorf("server sent more data than expected, expected %d, got %d", downloadBytes, remaining+uint64(n))
 		}
 		remaining -= uint64(n)
 		if err != nil {
@@ -65,10 +76,10 @@ func handleClientStream(str io.ReadWriteCloser, uploadBytes, downloadBytes uint6
 				if remaining == 0 {
 					break
 				}
-				return fmt.Errorf("server didn't send enough data, expected %d, got %d", downloadBytes, downloadBytes-remaining)
+				return 0, 0, fmt.Errorf("server didn't send enough data, expected %d, got %d", downloadBytes, downloadBytes-remaining)
 			}
-			return err
+			return 0, 0, err
 		}
 	}
-	return nil
+	return uploadTook, time.Since(downloadStart), nil
 }
---
+++ util.go
@@ -0,0 +1,23 @@
+package perf
+
+import (
+	"fmt"
+	"time"
+)
+
+func bandwidth(b uint64, d time.Duration) uint64 {
+	return uint64(float64(b) / d.Seconds())
+}
+
+func formatBytes(b uint64) string {
+	const unit = 1024
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %ciB", float64(b)/float64(div), "KMGTPE"[exp])
+}
