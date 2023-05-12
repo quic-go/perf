33f3596cb2763187967a45baed4584e258b4e953
Max Inden <mail@max-inden.de>
2023-05-12 18:13:49 +0900

    Ignore unknown flags

--- cmd/main.go
+++ cmd/main.go
@@ -2,41 +2,49 @@ package main
 
 import (
 	"crypto/tls"
-	"flag"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
 	"os"
 
+	"github.com/jessevdk/go-flags"
 	"github.com/quic-go/perf"
 )
+
+type Options struct {
+	RunServer      bool    `long:"run-server" description:"run as server, default: false"`
+	ServerAddress  string  `long:"server-address" description:"server address, required"`
+	UploadBytes    uint64  `long:"upload-bytes" description:"upload bytes"`
+	DownloadBytes  uint64  `long:"download-bytes" description:"download bytes"`
+}
 
 var tlsConf *tls.Config
 
 func main() {
-	server := flag.Bool("run-server", false, "run as server, default: false")
-	serverAddr := flag.String("server-address", "", "server address, required")
-	uploadBytes := flag.Uint64("upload-bytes", 0, "upload bytes")
-	downloadBytes := flag.Uint64("download-bytes", 0, "download bytes")
-	flag.Parse()
+	var opt Options
+	parser := flags.NewParser(&opt, flags.IgnoreUnknown)
+	_, err := parser.Parse()
+	if err != nil {
+		panic(err)
+	}
 
-	if *serverAddr == "" {
-		flag.Usage()
+	if opt.ServerAddress == "" {
+		parser.WriteHelp(os.Stdout)
 		os.Exit(1)
 	}
 
-	if *server {
+	if opt.RunServer {
 		go func() {
 			log.Println(http.ListenAndServe("0.0.0.0:6060", nil))
 		}()
-		if err := perf.RunServer(*serverAddr); err != nil {
+		if err := perf.RunServer(opt.ServerAddress); err != nil {
 			log.Fatal(err)
 		}
 	} else {
 		go func() {
 			log.Println(http.ListenAndServe("0.0.0.0:6061", nil))
 		}()
-		if err := perf.RunClient(*serverAddr, *uploadBytes, *downloadBytes); err != nil {
+		if err := perf.RunClient(opt.ServerAddress, opt.UploadBytes, opt.DownloadBytes); err != nil {
 			log.Fatal(err)
 		}
 	}
