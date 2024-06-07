4f417e238ed9361ad15754666bc720ca7db952c6
Marten Seemann <martenseemann@gmail.com>
2024-06-07 10:50:44 +0800

    enable support for qlog (controlled by QLOGDIR env) (#25)

--- perf.go
+++ perf.go
@@ -1,6 +1,9 @@
 package perf
 
-import "github.com/quic-go/quic-go"
+import (
+	"github.com/quic-go/quic-go"
+	"github.com/quic-go/quic-go/qlog"
+)
 
 const ALPN = "perf"
 
@@ -8,4 +11,5 @@ var config = &quic.Config{
 	// use massive flow control windows here to make sure that flow control is not the limiting factor
 	MaxConnectionReceiveWindow: 1 << 30,
 	MaxStreamReceiveWindow:     1 << 30,
+	Tracer:                     qlog.DefaultConnectionTracer,
 }
