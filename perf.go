7cb7af06d96fc5b7f81f62420b24b607cded44c5
Marten Seemann <martenseemann@gmail.com>
2025-08-24 15:55:50 +0800

    increase the initial flow control window to 1 MB

--- perf.go
+++ perf.go
@@ -9,7 +9,9 @@ const ALPN = "perf"
 
 var config = &quic.Config{
 	// use massive flow control windows here to make sure that flow control is not the limiting factor
-	MaxConnectionReceiveWindow: 1 << 30,
-	MaxStreamReceiveWindow:     1 << 30,
-	Tracer:                     qlog.DefaultConnectionTracer,
+	InitialStreamReceiveWindow:     1 << 20,
+	InitialConnectionReceiveWindow: 1 << 20,
+	MaxConnectionReceiveWindow:     1 << 30,
+	MaxStreamReceiveWindow:         1 << 30,
+	Tracer:                         qlog.DefaultConnectionTracer,
 }
